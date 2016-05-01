64eedd603b5ceb676c4d3f0e7015ad543c97cdaa
Andrew Taranik <andrew.taranik@gmail.com>
2016-05-01 16:55:02 +0000

    simplefied and added secure more for manager

--- build_cfy.sh
+++ build_cfy.sh
@@ -1,18 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# Update and install dependicies
-
-sudo yum -y -q update
-sudo yum -y -q install wget unzip curl nano which gcc
-sudo yum -y -q install python python-setuptools python-devel
-sudo easy_install --quiet --upgrade pip virtualenv
-
-# Install CFY 3.3.1
 cd \
-  && wget -q http://repository.cloudifysource.org/org/cloudify3/3.3.1/sp-RELEASE/cloudify-centos-Core-cli-3.3.1-sp_b310.x86_64.rpm \
-  && sudo rpm --quiet -i cloudify-centos-Core-cli-3.3.1-sp_b310.x86_64.rpm \
-  && rm -f cloudify-centos-Core-cli-3.3.1-sp_b310.x86_64.rpm \
+  && sudo easy_install --quiet --upgrade pip virtualenv \
+  && curl -sSL http://repository.cloudifysource.org/org/cloudify3/3.3.1/sp-RELEASE/cloudify-centos-Core-cli-3.3.1-sp_b310.x86_64.rpm -o cfy.rpm \
+  && sudo rpm --quiet -i cfy.rpm \
+  && rm cfy.rpm \
   && echo "source /opt/cfy/env/bin/activate" >>~/.bashrc \
-  && sudo yum -y -q clean all \
-  && echo -e "\nRelogin or enter:  source /opt/cfy/env/bin/activate\n" \
-  || echo -e "\nWARNING! Something wrong\n"
+  && sudo yum -y -q clean all
--- build_cfy_manager.sh
+++ build_cfy_manager.sh
@@ -4,5 +4,7 @@ if [ ! -d .cloudify ]; then cfy init; fi
 
 sudo cp -f own-openstack-manager-blueprint.yaml /opt/cfy/cloudify-manager-blueprints-commercial/
 
-cfy bootstrap --install-plugins -p /opt/cfy/cloudify-manager-blueprints-commercial/own-openstack-manager-blueprint.yaml -i own-openstack-manager-blueprint-inputs.yaml
+export CLOUDIFY_USERNAME=admin
+export CLOUDIFY_PASSWORD=`grep admin_password own-openstack-manager-blueprint-inputs.yaml | awk '{print $2;}'`
 
+cfy bootstrap --install-plugins -p /opt/cfy/cloudify-manager-blueprints-commercial/own-openstack-manager-blueprint.yaml -i own-openstack-manager-blueprint-inputs.yaml
